bb3b93b481a7e70f5d42d3e09dc814bb5c7b1bd5
ssiniorg <sandersiniorg@gmail.com>
2017-09-07 14:49:20 +0300

    config removed

--- imperial/src/fire.js
+++ imperial/src/fire.js
@@ -1,14 +1,5 @@
 import firebase from 'firebase'
 
-const config = {
-    apiKey: "AIzaSyDFJvGMawlU51blOHFy-5173Cj6Q6F2mVk",
-    authDomain: "imperial-d5146.firebaseapp.com",
-    databaseURL: "https://imperial-d5146.firebaseio.com",
-    projectId: "imperial-d5146",
-    storageBucket: "",
-    messagingSenderId: "288385523341"
-};
-
 export const firebaseApp = firebase.initializeApp(config);
 
 export const db = firebaseApp.database();
